ceae50a518ac68b33b234ff7e5d89f4938b270f4
Yassine Zayet <yassine50.zayet@gmail.com>
2023-06-10 16:28:07 +0100

    modeling data v2

--- models/tourModel.js
+++ models/tourModel.js
@@ -120,6 +120,13 @@ tourSchema.virtual('durationWeeks').get(function() {
   return this.duration / 7;
 });
 
+//virtual populate
+tourSchema.virtual('reviews', {
+  ref: 'Review',
+  foreignField: 'tour',
+  localField: '_id'
+});
+
 // DOCUMENT MIDDLEWARE: runs before .save() and .create()
 tourSchema.pre('save', function(next) {
   this.slug = slugify(this.name, { lower: true });
@@ -158,12 +165,11 @@ tourSchema.post(/^find/, function(docs, next) {
 
 tourSchema.pre(/^find/, function(next) {
   this.populate({
-    path:'guides',
-    select:'-__v -passwordChangedAt'
-  })
+    path: 'guides',
+    select: '-__v -passwordChangedAt'
+  });
   next();
-})
-
+});
 
 // AGGREGATION MIDDLEWARE
 tourSchema.pre('aggregate', function(next) {
--- routes/reviewRoutes.js
+++ routes/reviewRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const reviewController = require('../controllers/reviewController');
 const authController = require('../controllers/authController');
-const router = express.Router();
+const router = express.Router({ mergeParams: true });
 
 router
   .route('/')
@@ -11,5 +11,7 @@ router
     authController.restrictTo('user'),
     reviewController.createReview
   );
+
+router.route('/:id').delete(reviewController.deleteReview);
 
 module.exports = router;
