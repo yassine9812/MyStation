6b2261780a5674240db57b34903b01769699f8f6
Yassine Zayet <yassine50.zayet@gmail.com>
2023-06-11 17:44:03 +0100

    modeling data v3

--- controllers/userController.js
+++ controllers/userController.js
@@ -11,19 +11,6 @@ const filterObj = (obj, ...allowedFields) => {
   return newObj;
 };
 
-exports.getAllUsers = catchAsync(async (req, res, next) => {
-  const users = await User.find();
-
-  // SEND RESPONSE
-  res.status(200).json({
-    status: 'success',
-    results: users.length,
-    data: {
-      users
-    }
-  });
-});
-
 exports.updateMe = catchAsync(async (req, res, next) => {
   // 1) Create error if user POSTs password data
   if (req.body.password || req.body.passwordConfirm) {
@@ -61,16 +48,10 @@ exports.deleteMe = catchAsync(async (req, res, next) => {
   });
 });
 
-exports.getUser = (req, res) => {
-  res.status(500).json({
-    status: 'error',
-    message: 'This route is not yet defined!'
-  });
-};
 exports.createUser = (req, res) => {
   res.status(500).json({
     status: 'error',
-    message: 'This route is not yet defined!'
+    message: 'This route is not yet defined Please use sign up instead!'
   });
 };
 exports.updateUser = (req, res) => {
@@ -79,4 +60,10 @@ exports.updateUser = (req, res) => {
     message: 'This route is not yet defined!'
   });
 };
+
+exports.getUser = factory.getOne(User);
+exports.getAllUsers = factory.getAll(User);
+
+//DO NOI UPDATE PASSWORD WITH THIS
 exports.deleteUser = factory.deletOne(User);
+exports.updateUser = factory.updateOne(User);
